6f963b0cbf70ba26d3a18da7d00544d52347ae56
Georgios Kousalidis <kousgeor@csd.auth.gr>
2022-05-13 13:19:07 +0300

    Add empty project

---
+++ Dodge game/Assets/Scripts/Dropper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    MeshRenderer myRenderer;
+    Rigidbody myRigidBody;
+    [SerializeField] float timeToWait = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRenderer = GetComponent<MeshRenderer>();
+        myRigidBody = GetComponent<Rigidbody>();
+
+        myRenderer.enabled = false;
+        myRigidBody.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Time.time);
+
+        if (Time.time > timeToWait)
+        {
+            //Debug.Log(timeToWait + " seconds has elapsed");
+
+            myRenderer.enabled = true;
+            myRigidBody.useGravity = true;
+        }
+    }
+}
---
+++ Dodge game/Assets/Scripts/Mover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    // these variable change our movement speed 
+    // [SerializeField] float xValue = 0;
+    // [SerializeField] float zValue = 0;
+    // [SerializeField] float yValue = 0;
+
+    [SerializeField] float moveSpeed = 26f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintInstructions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls");
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        
+        transform.Translate(xValue,0,zValue);
+    }
+}
